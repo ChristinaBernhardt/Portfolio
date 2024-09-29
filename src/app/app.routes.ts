2ab263aa6aa92b216b940f73b44a132091dd1645
Christina Bernhardt <sozia76@web.de>
2024-09-29 21:40:50 +0200

    save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainComponent } from './maincontent/main.component';
+import { ContactComponent } from './maincontent/contact/contact.component';
+
 
 export const routes: Routes = [
     { path: '', component: MainComponent},
     { path: 'privacy', component: PrivacyComponent},
     { path: 'imprint', component: ImprintComponent},
+    { path: 'contact', component: ContactComponent },
 
 ];
